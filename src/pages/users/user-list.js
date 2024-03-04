e664c17296aa112082c06b800d43092b5f555d67
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-03-04 14:03:40 +0530

    updated table

--- src/pages/users/user-list.js
+++ src/pages/users/user-list.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styles from './user-list.module.css'
 
 const UserList = () => {
   return (
     <div>
-      
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Created</th>
+          <th>Modified</th>
+        </tr>
+        <tr></tr>
+      </table>
     </div>
   )
 }
